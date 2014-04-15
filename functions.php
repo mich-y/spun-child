9b99541bcd65fce9c959aa274fa1fe31a22ee7af
mich-y <mjyong1@gmail.com>
2014-04-14 19:56:50 -0700

    First Commit
    All the changes that have been made so far.

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+// Enqueque Google Webfonts
+  function load_fonts() {
+            wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Alegreya+Sans:300,400,700,700italic|Alegreya:400italic,700italic,400,700');
+            wp_enqueue_style( 'googleFonts');
+        }
+    
+    add_action('wp_print_styles', 'load_fonts');
+
+?>
